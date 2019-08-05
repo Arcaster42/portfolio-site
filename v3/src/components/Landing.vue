776d7920ce4b0ad2f6e0cb402d8c30f29434710c
TJ Epperson <tj.epperson070@gmail.com>
2019-08-05 21:49:47 +0900

    Started v3

---
+++ v3/src/components/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="landing-div">
+        <img class="landing-img" src="../assets/in_landing.jpg">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'landing'    
+}
+</script>
+
+<style scoped>
+.landing-div {
+    margin: 0px;
+}
+
+.landing-img {
+    height: 105%;
+    width: 105%;
+    position: absolute;
+    margin: -10px;
+}
+</style>
