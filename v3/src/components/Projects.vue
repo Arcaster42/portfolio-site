2faee7752213b7fc95d2f9840435f0d4118cba34
TJ Epperson <tj.epperson070@gmail.com>
2019-08-07 08:56:30 +0900

    Added background images

--- v3/src/components/Projects.vue
+++ v3/src/components/Projects.vue
@@ -2,8 +2,8 @@
     <div>
         <div class="spacer">
         </div>
-        <div class="project-div" v-for="project of projects" :key="project.name">
-            <span class="project-span" v-bind:id="project.name">{{project.name}}</span>
+        <div class="project-div">
+            <span class="project-span" v-for="project of projects" :key="project.name" v-bind:id="project.name" v-bind:style="{backgroundImage: 'url(' + project.image + ')'}">{{project.name}}</span>
         </div>
     </div>
 </template>
@@ -18,6 +18,12 @@ export default {
                     name: 'rhymus',
                     release: 'open-source',
                     description: 'An open-source game developed in JavaScript and refactored in both React and Vue.',
+                    image: './'
+                },
+                {
+                    name: 'portfolio',
+                    release: 'public',
+                    description: 'This website written over three iterations, the latest using Vue.',
                     image: './'
                 }
             ]
@@ -35,5 +41,25 @@ export default {
     width: 100%;
     background-color: darkblue;
     color: ghostwhite;
+}
+
+.project-div {
+    text-align: center;
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+}
+
+.project-span {
+    display: flex;
+    float: left;
+    height: 100px;
+    width: 300px;
+    border: 2px solid black;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
 }
 </style>
